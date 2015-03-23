597c0a8b84316c0de8751faf0fe3b94eeef884ec
cneha1991 <cneha1991@gmail.com>
2015-03-23 21:17:36 +0100

    UnitTest and images

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,3 +1,4 @@
+package controllers;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -14,9 +15,27 @@ import play.data.validation.Constraints.RequiredValidator;
 import play.i18n.Lang;
 import play.libs.F;
 import play.libs.F.*;
+import play.libs.Json;
 
 import static play.test.Helpers.*;
 import static org.fest.assertions.Assertions.*;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static play.mvc.Http.Status.OK;
+import static play.test.Helpers.GET;
+import static play.test.Helpers.callAction;
+import static play.test.Helpers.charset;
+import static play.test.Helpers.contentAsString;
+import static play.test.Helpers.contentType;
+import static play.test.Helpers.status;
+import controllers.Application;
+
+import org.junit.Test;
+
+import play.mvc.Result;
+import play.test.FakeRequest;
+
+import com.fasterxml.jackson.databind.JsonNode;
 
 
 /**
@@ -39,6 +58,68 @@ public class ApplicationTest {
         assertThat(contentType(html)).isEqualTo("text/html");
         assertThat(contentAsString(html)).contains("Your new application is ready.");
     }
+    
+        @Test
+        public void testConnection(){
+        	FakeApplication fakeApplication=fakeApplication();
+        	start(fakeApplication);
+        	String body = "{\"hostname\":\"127.0.0.1\", \"port\": \"9042\"}";
+            JsonNode json = Json.parse(body);
+            FakeRequest request = new FakeRequest(POST, "/connection").withJsonBody(json);
+            Result result = callAction(controllers.routes.ref.Application.connectToCassandra(), request);
+            assertThat(status(result)).isEqualTo(OK);
+            assertThat(contentType(result)).isEqualTo("application/json");
+            assertThat(contentAsString(result)).contains("connected Successfully");
+            stop(fakeApplication);
+        }
+        
+        @Test
+        public void testDropKeyspace(){
+        	FakeApplication fakeApplication=fakeApplication();
+        	start(fakeApplication);
+        	String nameKeyspace ="UnitTest";
+        	String body = "{\"keyspacename\":\"UnitTest\", \"kclass\":\"SimpleStrategy\", \"replication_factor\":3}";
+            JsonNode json = Json.parse(body);
+            FakeRequest request = new FakeRequest(DELETE, "/keyspace/"+nameKeyspace);
+            Result result = callAction(controllers.routes.ref.Application.deleteKeyspace(nameKeyspace), request);
+            assertThat(status(result)).isEqualTo(OK);
+            assertThat(contentType(result)).isEqualTo("application/json");
+            assertThat(contentAsString(result)).contains("");
+            stop(fakeApplication);
+        }
+        
+        // Unit test create keyspace
+        @Test
+        public void testCreateKeyspace(){
+        	FakeApplication fakeApplication=fakeApplication();
+        	start(fakeApplication);
+        	String body = "{\"keyspacename\":\"UnitTest\", \"kclass\":\"SimpleStrategy\", \"replication_factor\":3}";
+            JsonNode json = Json.parse(body);
+            FakeRequest request = new FakeRequest(POST, "/keyspace").withJsonBody(json);
+            Result result = callAction(controllers.routes.ref.Application.createKeyspace(), request);
+            assertThat(status(result)).isEqualTo(OK);
+            assertThat(contentType(result)).isEqualTo("application/json");
+            assertThat(contentAsString(result)).contains("Added New Keyspace");
+            stop(fakeApplication);
+        }
+        
+        //Unit Test Keyspace Schema
+        @Test
+        public void testKeyspaceSchema(){
+        	FakeApplication fakeApplication=fakeApplication();
+        	start(fakeApplication);
+        	//String body = "{\"keyspacename\":\"UnitTest\", \"kclass\":\"SimpleStrategy\", \"replication_factor\":3}";
+            //JsonNode json = Json.parse(body);
+            FakeRequest request = new FakeRequest(GET, "/keyspace/demo/table");
+            Result result = callAction(controllers.routes.ref.Application.getTableSchema("demo"), request);
+            assertThat(status(result)).isEqualTo(OK);
+            assertThat(contentType(result)).isEqualTo("application/json");
+            assertThat(contentAsString(result)).contains("{\"columnfamily_name\":\"users\"}");
+            stop(fakeApplication);
+        }
+
+
+
 
 
 }
